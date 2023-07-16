8e38037cd79371d361915389c19194746a69ff6a
slukin1 <slukin@ukr.net>
2023-07-16 21:07:39 +0300

    linter

--- components/header/header.js
+++ components/header/header.js
@@ -1,35 +1,35 @@
-import ColorHeaderTop from '../components/header/colorheadertop';
+import ColorHeaderTop from "../components/header/colorheadertop";
 
-import Link from 'next/link';
+import Link from "next/link";
 
 const Header = () => (
   <header className="main">
-  <ColorHeaderTop>
-    <ul>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/about">About</Link>
-      </li>
-      <li>
-        <Link
-          href="/post/[...slug]"
-          as="/post/2020/first-post/with/catch/all/routes"
-        >
-          First Post
-        </Link>
-      </li>
-      <li>
-        <Link
-          href="/post/[...slug]"
-          as="/post/2020/second-post/with/catch/all/routes"
-        >
-          Second Post
-        </Link>
-      </li>
-    </ul>
-            </ColorHeaderTop >
+    <ColorHeaderTop>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link
+            href="/post/[...slug]"
+            as="/post/2020/first-post/with/catch/all/routes"
+          >
+            First Post
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/post/[...slug]"
+            as="/post/2020/second-post/with/catch/all/routes"
+          >
+            Second Post
+          </Link>
+        </li>
+      </ul>
+    </ColorHeaderTop>
   </header>
 );
 
